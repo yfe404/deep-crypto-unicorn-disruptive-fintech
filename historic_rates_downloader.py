d23d4f250e19df69526ee8eb5941f69819998509
Maxime Mouchet <max@maxmouchet.com>
2017-05-31 22:55:42 +0200

    Draft historic rates downloader

---
+++ historic_rates_downloader.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import os, sys, json, requests
+from datetime import datetime, timedelta
+from coinbase import CoinbaseExchangeAuth
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+API_URL = 'https://api.gdax.com/'
+
+API_KEY = os.environ['GDAX_API_KEY']
+API_SECRET = os.environ['GDAX_API_SECRET']
+API_PASS = os.environ['GDAX_API_PASS']
+
+if len(sys.argv) is not 5:
+    eprint('Usage: {} START_DATE END_DATE GRANULARITY PRODUCT'.format(sys.argv[0]))
+    eprint('Example: {} 2017-01-01T06:00:00 2017-01-31T23:00:00 30 BTC-USD'.format(sys.argv[0]))
+    eprint('Date is an ISO8601 string and granularity is in seconds.')
+    exit(1)
+
+start_date = datetime.strptime(sys.argv[1],'%Y-%m-%dT%H:%M:%S')
+end_date = datetime.strptime(sys.argv[2],'%Y-%m-%dT%H:%M:%S')
+granularity = int(sys.argv[3])
+product = sys.argv[4]
+
+eprint('Downloading historic rates between {} and {} with granularity of {} seconds'.format(start_date, end_date, granularity))
+
+auth = CoinbaseExchangeAuth(API_KEY, API_SECRET, API_PASS)
+result = []
+
+# NOTE: API limit is 200 data points per request
+delta = timedelta(seconds=200*granularity)
+current_date = start_date
+while current_date < end_date:
+    intermediate_date = current_date + delta
+    eprint('Downloading rates between {} and {} ...'.format(current_date, intermediate_date))
+    params = {'start': current_date.isoformat(), 'end': intermediate_date.isoformat(), 'granularity': granularity}
+    r = requests.get(API_URL + 'products/{}/candles'.format(product), params=params, auth=auth)
+    # TODO: Handle exception
+    result.extend(r.json())
+    current_date = intermediate_date
+
+# Output CSV
+print('time,low,high,open,close,volume')
+for rates in result:
+    print('{},{},{},{},{},{}'.format(rates[0], rates[1], rates[2], rates[3], rates[4], rates[5]))
+
